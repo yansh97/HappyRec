a611aebe367dd83685b772bd1d854108e8be44da
YanSH <yansh97@foxmail.com>
2022-11-13 18:50:41 +0800

    feat(data.ColType): add data.ColType and some functions.

---
+++ happyrec/data/__init__.py
@@ -0,0 +1,16 @@
+from .column import (
+    ColType,
+    generate_col_name,
+    get_col_info,
+    get_col_type,
+    get_feat_name,
+)
+
+__all__ = [
+    # Column
+    "ColType",
+    "get_col_info",
+    "get_feat_name",
+    "get_col_type",
+    "generate_col_name",
+]
